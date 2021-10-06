b82510029c0b64a1f99d58ea58138ca1949f6e60
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2021-10-06 09:22:20 +0200

    wip: metadata experiment

--- src/qtools/data/base.py
+++ src/qtools/data/base.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from typing import Optional
+from qcodes.dataset.experiment_container import Experiment as qcExperiment
 
 from qtools.data.device import Wafer, Device, Sample, Design
+from qtools.data.measurement import Experiment as metaExperiment, MeasurementType
 
 
 def create_metadata_device() -> Device:
@@ -16,3 +19,23 @@ def create_metadata_device() -> Device:
     design = Design.load_from_db("7d333741-d52e-4237-aa31-66869f1bbcce")
     device = Device.create("Device6", design, sample)
     return device
+
+
+def create_metadata_from_experiment(exp: qcExperiment = None) -> metaExperiment:
+    """
+    Creates the experiment metadata structure from QCoDeS.
+    It is expected, that the experiment has been created/loaded in QCoDeS.
+
+    Args:
+        exp: QCoDeS experiment object.
+
+    Returns:
+        Experiment: Experiment metadata
+    """
+    name = exp.name if exp else ""
+    desc = ""
+    user = ""
+    group = ""
+    measurement_type = MeasurementType.load_from_db("pid")
+    filters = ""
+    return metaExperiment.create(name, desc, user, group, measurement_type, filters, [])
