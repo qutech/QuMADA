ceb3bf6878882058fc7a195ea5a1f0ce6636045b
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2021-10-11 13:19:21 +0200

    fix api calls

--- src/qtools/data/base.py
+++ src/qtools/data/base.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Optional
 from qcodes.dataset.experiment_container import Experiment as qcExperiment
 
-from qtools.data.device import Wafer, Device, Sample, Design
-from qtools.data.measurement import Experiment as metaExperiment, MeasurementType
+from qtools.data.device import Design, Device, Sample, Wafer
+from qtools.data.measurement import Experiment as metaExperiment
+from qtools.data.measurement import MeasurementType
 
 
 def create_metadata_device() -> Device:
@@ -14,9 +14,9 @@ def create_metadata_device() -> Device:
     Returns:
         Device: Testdevice
     """
-    wafer = Wafer.load_from_db("081b0ae8-e0e1-45f9-84e7-9779340343b4")
+    wafer = Wafer.get_by_id("081b0ae8-e0e1-45f9-84e7-9779340343b4")
     sample = Sample.create("S4", "Testsample 4", wafer)
-    design = Design.load_from_db("7d333741-d52e-4237-aa31-66869f1bbcce")
+    design: Design = Design.get_by_id("7d333741-d52e-4237-aa31-66869f1bbcce")
     device = Device.create("Device6", design, sample)
     return device
 
