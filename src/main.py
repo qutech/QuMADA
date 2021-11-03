7508bfe669ba9df203fd3c0cbe5ad271ac1d7549
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2021-11-03 14:57:46 +0100

    add cmd2 and implement basic metadata commands

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+import pprint
 from typing import Any, MutableMapping
 
 import qcodes as qc
 import qcodes.instrument.sims as qcsims
 import yaml
+from cmd2 import Cmd, Cmd2ArgumentParser, with_argparser
 from qcodes.instrument.base import Instrument
 from qcodes.instrument_drivers.Harvard.Decadac import Decadac
 from qcodes.instrument_drivers.stanford_research.SR830 import SR830
@@ -44,6 +46,89 @@ from qtools.instrument.mapping import (
     KEITHLEY_2450_MAPPING,
     SR830_MAPPING,
 )
+
+# Cmd2 Parsers
+metadata_parser = Cmd2ArgumentParser()
+metadata_subparsers = metadata_parser.add_subparsers(
+    title="subcommands", help="subcommand help"
+)
+
+parser_metadata_load = metadata_subparsers.add_parser(
+    "load", help="Load a metadata object from a YAML-file."
+)
+parser_metadata_load.add_argument(
+    "file",
+    metavar="FILE",
+    type=argparse.FileType("r"),
+    help="YAML-file with metadata information.",
+)
+
+parser_metadata_new = metadata_subparsers.add_parser(
+    "new", help="Create an empty metadata object."
+)
+
+parser_metadata_print = metadata_subparsers.add_parser(
+    "print", help="print the metadata."
+)
+parser_metadata_print.add_argument(
+    "-f", "--format", choices=["base", "yaml"], default="base", help="Output format."
+)
+parser_metadata_print.add_argument(
+    "-o",
+    "--output",
+    type=argparse.FileType("w"),
+    default="-",
+    help="Output file to write the metadata to.",
+)
+
+
+class QToolsApp(Cmd):
+    def __init__(self):
+        super().__init__()
+
+        # remove cmd2 builtin commands
+        del Cmd.do_edit
+        del Cmd.do_shell
+        # hide cmd2 builtin commands
+        self.hidden_commands.append("alias")
+        self.hidden_commands.append("macro")
+        self.hidden_commands.append("run_pyscript")
+        self.hidden_commands.append("run_script")
+        self.hidden_commands.append("shortcuts")
+
+        # Metadata
+        db.api_url = "http://134.61.7.48:9123"
+        self.metadata = Metadata()
+        self.station = Station()
+
+    def metadata_load(self, args):
+        try:
+            self.metadata = Metadata.from_yaml(args.file)
+        except ValueError as e:
+            self.pexcept(e)
+
+    def metadata_new(self, args):
+        self.metadata = Metadata()
+
+    def metadata_print(self, args):
+        if args.format == "base":
+            # This output gets better with Python 3.10 (pprint support for dataclass)
+            pprint.pp(self.metadata, stream=args.output)
+        elif args.format == "yaml":
+            yaml.dump(self.metadata, stream=args.output)
+
+    parser_metadata_load.set_defaults(func=metadata_load)
+    parser_metadata_new.set_defaults(func=metadata_new)
+    parser_metadata_print.set_defaults(func=metadata_print)
+
+    @with_argparser(metadata_parser)
+    def do_metadata(self, args):
+        """metadata command branching"""
+        func = getattr(args, "func", None)
+        if func:
+            func(self, args)
+        else:
+            self.do_help("metadata")
 
 
 def _initialize_instruments() -> MutableMapping[Any, Instrument]:
@@ -85,35 +170,9 @@ def _initialize_instruments() -> MutableMapping[Any, Instrument]:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("qtools")
-    parser.add_argument(
-        "-m",
-        "--metadata",
-        type=argparse.FileType("r"),
-        help="YAML-file with metadata information.",
-    )
-    # Disable metadata
-    parser.add_argument(
-        "--no-metadata", "-nm", action="store_true", help="Disable metadata."
-    )
-
-    args = parser.parse_args()
-
-    # Load metadata
-    if not args.no_metadata:
-        db.api_url = "http://134.61.7.48:9123"
-        try:
-            with args.metadata or open("metadata.yaml") as f:
-                metadata = Metadata.from_yaml(f)
-            metadata.save_to_db()
-            # update metadata.yaml with pids
-            with args.metadata or open("metadata.yaml") as f:
-                yaml.dump(metadata, f, yaml.Dumper)
-        except FileNotFoundError:
-            print(
-                "No Metadata file found. Please specify using -m/--metadata [filename].yaml or provide a default metadata.yaml in the working directory."
-            )
-            raise SystemExit(1)
+    app = QToolsApp()
+    ret_code = app.cmdloop()
+    raise SystemExit(ret_code)
 
     # Create station with instruments
     station = Station()
