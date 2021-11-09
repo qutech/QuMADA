8a210c81427a8f9b5b4d852a90043dda5caeb27b
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2021-11-09 13:20:14 +0100

    completion for instrument drivers

---
+++ src/qtools/utils/import_submodules.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import importlib
+import pkgutil
+from types import ModuleType
+
+
+def import_submodules(
+    package: str | ModuleType, recursive: bool = True
+) -> dict[str, ModuleType]:
+    """Import all submodules of a module, recursively, including subpackages"""
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + "." + name
+        try:
+            results[full_name] = importlib.import_module(full_name)
+        except ImportError as e:
+            print(f"ImportError for {full_name}: {e}")
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
