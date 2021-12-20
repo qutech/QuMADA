fe3b9aa868c09e3bb620c214a23adaf9409a6215
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2021-12-20 13:54:16 +0100

    fill joblist commands

--- src/qtools/ui/cmd2/parsers/joblist_parser.py
+++ src/qtools/ui/cmd2/parsers/joblist_parser.py
@@ -3,8 +3,13 @@ import pprint
 
 from cmd2 import Cmd2ArgumentParser, CommandSet, with_argparser
 
+from qtools.measurement.jobs import Job, Joblist
+
 
 class JoblistCommandSet(CommandSet):
+    def choices_joblist_jobs(self) -> list[str]:
+        return [str(job) for job in self._cmd.joblist]
+
     # Joblist parsers
     joblist_parser = Cmd2ArgumentParser()
     joblist_subparsers = joblist_parser.add_subparsers(
@@ -27,10 +32,22 @@ class JoblistCommandSet(CommandSet):
     parser_joblist_add = joblist_subparsers.add_parser(
         "add", help="Adds the current job to the joblist."
     )
+    parser_joblist_add.add_argument(
+        "-c",
+        "--clear",
+        action="store_true",
+        help="Clear the current job after adding it to the joblist.",
+    )
 
     # joblist delete
     parser_joblist_delete = joblist_subparsers.add_parser(
         "delete", help="Removes a job from the joblist."
+    )
+    parser_joblist_delete.add_argument(
+        "job",
+        metavar="JOB",
+        help="Job to delete.",
+        choices_provider=choices_joblist_jobs,
     )
 
     # joblist clear
@@ -48,16 +65,24 @@ class JoblistCommandSet(CommandSet):
         pprint.pp(self._cmd.joblist, stream=args.output)
 
     def joblist_add(self, args):
-        ...
+        job: Job = self._cmd.current_job
+        joblist: Joblist = self._cmd.joblist
+        joblist.append(job)
+        if args.clear:
+            self._cmd.current_job = Job()
 
     def joblist_delete(self, args):
-        ...
+        raise NotImplementedError()
 
     def joblist_clear(self, args):
         self._cmd.joblist.clear()
 
     def joblist_run(self, args):
-        ...
+        joblist: Joblist = self._cmd.joblist
+        for job in joblist:
+            assert isinstance(job, Job)
+            job._script.run()
+        joblist.clear()
 
     # function mapping
     parser_joblist_print.set_defaults(func=joblist_print)
