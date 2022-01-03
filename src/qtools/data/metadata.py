qutech/QuMADA

commit ba2c158051348c569711702f048bf5e5d89772ed

have only Measurement object on base of metadata.yaml

src/qtools/data/metadata.py
@@ -12,8 +12,6 @@ from qtools.data.measurement import Experiment, Measurement
 class Metadata(yaml.YAMLObject):
     yaml_tag = "!Metadata"
 
-    device: Device
-    experiment: Experiment
     measurement: Measurement
 
     @classmethod

metadata.yaml
@@ -1,28 +1,26 @@
 !!python/object:qtools.data.metadata.Metadata
-device: &id001 !Device "63923883-1c47-412f-a1e7-1be411f90b1f"
-experiment: &id002 !Experiment
-  description: Testexperiment
-  equipmentInstances: null
-  group: group
-  measurementType: !!python/object:qtools.data.measurement.MeasurementType
-    equipments:
-    - !Equipment "d7914063-c786-4b0c-9ded-84390a3839ba"
-    - !Equipment "dbc9e518-2624-4536-b86d-1cc5ddb1c6bf"
-    - !Equipment "24e03597-fdca-4c70-ad39-3db752d3d636"
-    - !Equipment "57188264-4210-4877-b512-bab9086398b2"
-    extractableParameters: X
-    mapping: mapping
-    model: model
-    name: Measurementtype1
-    scriptTemplate: &id003 !MeasurementSettingScript "c52af245-fd65-4249-bcd2-367fe90cade1"
-  name: Experiment 1
-  softwareNoiseFilters: null
-  user: grothe
 measurement: !Measurement
-  device: *id001
-  experiment: *id002
+  device: !Device "63923883-1c47-412f-a1e7-1be411f90b1f"
+  experiment: !Experiment
+    description: Testexperiment
+    equipmentInstances: null
+    group: group
+    measurementType: !!python/object:qtools.data.measurement.MeasurementType
+      equipments:
+      - !Equipment "d7914063-c786-4b0c-9ded-84390a3839ba"
+      - !Equipment "dbc9e518-2624-4536-b86d-1cc5ddb1c6bf"
+      - !Equipment "24e03597-fdca-4c70-ad39-3db752d3d636"
+      - !Equipment "57188264-4210-4877-b512-bab9086398b2"
+      extractableParameters: X
+      mapping: mapping
+      model: model
+      name: Measurementtype1
+      scriptTemplate: &id001 !MeasurementSettingScript "c52af245-fd65-4249-bcd2-367fe90cade1"
+    name: Experiment 1
+    softwareNoiseFilters: null
+    user: grothe
   measurementParameters: meas_params
   name: Meas1
   settings: !MeasurementSettings
     name: MeasurementSetting1
-    script: *id003
+    script: *id001

commit bc26249b00e5fe6da43db3ab4e56f97051f924bf

fix metadata save bug
commit #ba2c1580 introduced a bug during save_to_db function

src/qtools/data/metadata.py
@@ -41,4 +41,4 @@ class Metadata(yaml.YAMLObject):
                     # No save function
                     pass
 
-        recurse([self.device, self.experiment, self.measurement])
+        recurse([self.measurement])