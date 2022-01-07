3748c0b51cb90c44192debfaf72e9134dec33eb2
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2022-01-07 13:40:04 +0100

    refactoring metadata.py

--- src/qtools/data/metadata.py
+++ src/qtools/data/metadata.py
@@ -1,15 +1,20 @@
+"""
+Measurement metadata
+"""
 from dataclasses import dataclass
 from typing import Iterable
 
 import yaml
 
-from qtools.data.device import Device
 from qtools.data.domain import DomainObject
-from qtools.data.measurement import Experiment, Measurement
+from qtools.data.measurement import Measurement
 
 
 @dataclass
 class Metadata(yaml.YAMLObject):
+    """
+    This metadata object can by loaded from YAML and it's content can be saved to the database.
+    """
     yaml_tag = "!Metadata"
 
     measurement: Measurement
@@ -23,15 +28,16 @@ class Metadata(yaml.YAMLObject):
             raise ValueError("Metadata file does not contain a metadata object.")
 
     def save_to_db(self):
+        """Save the measurement metadata to the database."""
         def recurse(node: Iterable[DomainObject]):
             for domain_object in node:
                 sublist = []
-                for v in domain_object.__dict__.values():
-                    if isinstance(v, DomainObject):
-                        sublist.append(v)
-                    elif isinstance(v, Iterable):
+                for value in domain_object.__dict__.values():
+                    if isinstance(value, DomainObject):
+                        sublist.append(value)
+                    elif isinstance(value, Iterable):
                         # TODO: I'm ugly, please make me prettier
-                        ssl = [o for o in v if isinstance(o, DomainObject)]
+                        ssl = [o for o in value if isinstance(o, DomainObject)]
                         sublist.extend(ssl)
                 if sublist:
                     recurse(sublist)
