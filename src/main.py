d2a0ec00392f8ca7bd33693cd749769d885c4dd0
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2022-01-14 15:39:11 +0100

    add qtools entrypoint and dependencies to setup.cfg
    now, qtools can be installed easily with an pip install . and started
    directly from command line using "qtools"

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@
 
 from qtools.ui.cmd2.app import QToolsApp
 
-if __name__ == "__main__":
+
+def start_console_app():
     app = QToolsApp()
     ret_code = app.cmdloop()
     raise SystemExit(ret_code)
+
+
+def start_gui_app():
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    start_console_app()
