d5af101f9d5c31036ed527951d42ffd569a37e2e
Sionludi Lab <sionludi@rwth.com>
2022-02-09 16:18:38 +0100

    Added parallel 1D sweeps

--- src/qtools/measurement/doNd_enhanced/doNd_enhanced.py
+++ src/qtools/measurement/doNd_enhanced/doNd_enhanced.py
@@ -3,12 +3,196 @@ Created on Mon Nov 29 12:57:40 2021
 
 @author: lab
 """
-from __future__ import annotations
 
+from __future__ import annotations
+import logging
+import sys
+import time
+import numpy as np
 import operator
 from functools import partial
-from typing import Any, Callable
+from typing import Callable, Dict, Iterator, List, Optional, Sequence, Tuple, Union, Any
+from qcodes.utils.threading import (
+    SequentialParamsCaller,
+    ThreadPoolParamsCaller,
+    process_params_meas,
+)
+from tqdm.auto import tqdm
+from qcodes import config
+import matplotlib
+from qcodes.dataset.experiment_container import Experiment
+from qcodes.dataset.measurements import Measurement
+from qcodes.dataset.descriptions.versioning.rundescribertypes import Shapes
+from qcodes.dataset.plotting import plot_dataset
+from qcodes.instrument.parameter import _BaseParameter
+from qcodes.dataset.data_set_protocol import DataSetProtocol, res_type
+from qcodes.dataset.descriptions.detect_shapes import detect_shape_of_measurement
+#from qcodes.utils.dataset.doNd import *
+from qcodes.utils.dataset.doNd import (_register_parameters,
+                                       _register_actions,
+                                       _set_write_period,
+                                       _catch_interrupts,
+                                       _handle_plotting,
+                                       UnsafeThreadingException,
+                                       BreakConditionInterrupt
+                                       )
 
+ActionsT = Sequence[Callable[[], None]]
+BreakConditionT = Callable[[], bool]
+
+ParamMeasT = Union[_BaseParameter, Callable[[], None]]
+
+AxesTuple = Tuple[matplotlib.axes.Axes, matplotlib.colorbar.Colorbar]
+AxesTupleList = Tuple[
+    List[matplotlib.axes.Axes], List[Optional[matplotlib.colorbar.Colorbar]]
+]
+AxesTupleListWithDataSet = Tuple[
+    DataSetProtocol,
+    List[matplotlib.axes.Axes],
+    List[Optional[matplotlib.colorbar.Colorbar]],
+]
+MultiAxesTupleListWithDataSet = Tuple[
+    Tuple[DataSetProtocol, ...],
+    Tuple[List[matplotlib.axes.Axes], ...],
+    Tuple[List[Optional[matplotlib.colorbar.Colorbar]], ...],
+]
+
+LOG = logging.getLogger(__name__)
+
+def do1d_parallel(
+    *param_meas: ParamMeasT,
+    param_set: list[ParamMeasT],
+    setpoints: np.array,
+    delay: float,
+    enter_actions: ActionsT = (),
+    exit_actions: ActionsT = (),
+    write_period: Optional[float] = None,
+    measurement_name: str = "",
+    exp: Optional[Experiment] = None,
+    do_plot: Optional[bool] = None,
+    use_threads: Optional[bool] = None,
+    additional_setpoints: Sequence[ParamMeasT] = tuple(),
+    show_progress: Optional[None] = None,
+    log_info: Optional[str] = None,
+    break_condition: Optional[BreakConditionT] = None,
+) -> AxesTupleListWithDataSet:
+    """
+    Perform a 1D scan of ``param_set`` from ``start`` to ``stop`` in
+    ``num_points`` measuring param_meas at each step. In case param_meas is
+    an ArrayParameter this is effectively a 2d scan.
+
+    Args:
+        param_set: The QCoDeS parameter to sweep over
+        start: Starting point of sweep
+        stop: End point of sweep
+        num_points: Number of points in sweep
+        delay: Delay after setting parameter before measurement is performed
+        *param_meas: Parameter(s) to measure at each step or functions that
+          will be called at each step. The function should take no arguments.
+          The parameters and functions are called in the order they are
+          supplied.
+        enter_actions: A list of functions taking no arguments that will be
+            called before the measurements start
+        exit_actions: A list of functions taking no arguments that will be
+            called after the measurements ends
+        write_period: The time after which the data is actually written to the
+            database.
+        additional_setpoints: A list of setpoint parameters to be registered in
+            the measurement but not scanned.
+        measurement_name: Name of the measurement. This will be passed down to
+            the dataset produced by the measurement. If not given, a default
+            value of 'results' is used for the dataset.
+        exp: The experiment to use for this measurement.
+        do_plot: should png and pdf versions of the images be saved after the
+            run. If None the setting will be read from ``qcodesrc.json`
+        use_threads: If True measurements from each instrument will be done on
+            separate threads. If you are measuring from several instruments
+            this may give a significant speedup.
+        show_progress: should a progress bar be displayed during the
+            measurement. If None the setting will be read from ``qcodesrc.json`
+        log_info: Message that is logged during the measurement. If None a default
+            message is used.
+
+    Returns:
+        The QCoDeS dataset.
+    """
+    if do_plot is None:
+        do_plot = config.dataset.dond_plot
+    if show_progress is None:
+        show_progress = config.dataset.dond_show_progress
+
+    meas = Measurement(name=measurement_name, exp=exp)
+    if log_info is not None:
+        meas._extra_log_info = log_info
+    else:
+        meas._extra_log_info = "Using 'qcodes.utils.dataset.doNd.do1d'"
+
+    all_setpoint_params = (param_set[0],) + tuple(s for s in additional_setpoints)
+
+    measured_parameters = tuple(
+        param for param in param_meas if isinstance(param, _BaseParameter)
+    )
+    measured_params = (*param_set[1:],*param_meas)
+    try:
+        loop_shape = tuple(1 for _ in additional_setpoints) + (len(setpoints),)
+        shapes: Shapes = detect_shape_of_measurement(
+            measured_params,
+            loop_shape
+        )
+        print(loop_shape)
+    except TypeError:
+        LOG.exception(
+            f"Could not detect shape of {measured_parameters} "
+            f"falling back to unknown shape.")
+        shapes = None
+    
+    _register_parameters(meas, all_setpoint_params, setpoints = None)
+    _register_parameters(meas, measured_params, setpoints=all_setpoint_params,
+                         shapes=shapes)
+    _set_write_period(meas, write_period)
+    _register_actions(meas, enter_actions, exit_actions)
+
+    original_delay = param_set[0].post_delay
+    param_set[0].post_delay = delay
+
+    if use_threads is None:
+        use_threads = config.dataset.use_threads
+
+    param_meas_caller = (
+        ThreadPoolParamsCaller(*param_meas)
+        if use_threads
+        else SequentialParamsCaller(*param_meas)
+    )
+    
+    # do1D enforces a simple relationship between measured parameters
+    # and set parameters. For anything more complicated this should be
+    # reimplemented from scratch
+    with _catch_interrupts() as interrupted, meas.run() as datasaver, param_meas_caller as call_param_meas:
+        dataset = datasaver.dataset
+        additional_setpoints_data = process_params_meas(additional_setpoints)
+
+        # flush to prevent unflushed print's to visually interrupt tqdm bar
+        # updates
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        for set_point in tqdm(setpoints, disable=not show_progress):
+            for param in param_set:
+                param.set(set_point)
+
+            datasaver.add_result(
+                (param_set[0], set_point),
+                *process_params_meas(measured_params, use_threads=use_threads),
+                *additional_setpoints_data
+            )
+            if callable(break_condition):
+                if break_condition():
+                    raise BreakConditionInterrupt("Break condition was met.")
+
+
+    param_set[0].post_delay = original_delay
+
+    return _handle_plotting(dataset, do_plot, interrupted())
 
 def _interpret_breaks(break_conditions: list, **kwargs) -> Callable[[], bool] | None:
     """
--- src/qtools/measurement/scripts/generic_measurement.py
+++ src/qtools/measurement/scripts/generic_measurement.py
@@ -5,33 +5,9 @@ import qcodes as qc
 from qcodes.dataset.experiment_container import load_or_create_experiment
 from qcodes.instrument import Parameter
 from qcodes.utils.dataset.doNd import LinSweep, do1d, do2d, dond
+from qtools.measurement.doNd_enhanced.doNd_enhanced import _interpret_breaks, do1d_parallel
 
-from qtools.measurement.doNd_enhanced.doNd_enhanced import _interpret_breaks
 from qtools.measurement.measurement import MeasurementScript
-
-# To be loaded from DB later on/Entered via GUI
-parameters = {
-    "topgate": {
-        "voltage": {
-            "type": "dynamic",
-            "start": 0,
-            "stop": 3,
-            "num_points": 0.1,
-            "delay": 0.01,
-        },
-        "current": {"type": "gettable"},
-        "output_enabled": {"type": "static", "value": True},
-        "current_compliance": {"type": "static", "value": 1e-9},
-    },
-    "source_drain": {
-        "amplitude": {"type": "static", "value": 1},
-        "frequency": {"type": "static", "value": 173},
-        "current": {"type": "gettable"},
-        "phase": {"type": "gettable"},
-    },
-    "left_barrier": {"voltage": {"type": "static", "value": 2}},
-    "right_barrier": {"voltage": {"type": "static", "value": 2}},
-}
 
 
 class Generic_1D_Sweep(MeasurementScript):
@@ -43,17 +19,20 @@ class Generic_1D_Sweep(MeasurementScript):
         self.initialize()
         data = list()
         time.sleep(5)
-        for sweep in self.dynamic_sweeps:
-            sweep.param.set(sweep.get_setpoints()[0])
+        i=0
+        for sweep in self.dynamic_sweeps:  
+            sweep._param.set(sweep.get_setpoints()[0])
+            time.sleep(5)
             data.append(
-                dond(
-                    sweep,
-                    *tuple(self.gettable_channels),
-                    measurement_name=self.metadata.measurement.name or "measurement",
-                    break_condition=_interpret_breaks(self.break_conditions)
+                dond(sweep,
+                     *tuple(self.gettable_channels),
+                     measurement_name=self.metadata.measurement.name or "measurement",
+                     #measurement_name=self.metadata.get('measurement_name', "measurement") +self.dynamic_parameters[i]["gate"],
+                     break_condition = _interpret_breaks(self.break_conditions)
+                     )
                 )
-            )
             self.reset()
+            i+=1
         return data
 
 
@@ -62,17 +41,38 @@ class Generic_nD_Sweep(MeasurementScript):
     Perform n-Dimensional sweep with n dynamic parameters.
     """
 
-    def run(self, parameters: dict, metadata: dict):
+    def run(self):
 
         self.initialize()
         for sweep in self.dynamic_sweeps:
             sweep._param.set(sweep.get_setpoints()[0])
         time.sleep(5)
-        data = dond(
-            *tuple(self.dynamic_sweeps),
-            *tuple(self.gettable_channels),
-            measurement_name=self.metadata.measurement.name or "measurement",
-            break_condition=_interpret_breaks(self.break_conditions)
-        )
+        data = dond(*tuple(self.dynamic_sweeps),
+                    *tuple(self.gettable_channels),
+                    measurement_name=self.metadata.measurement.name or "measurement",
+                    break_condition=_interpret_breaks(self.break_conditions)
+                     )
         self.reset()
         return data
+    
+class Generic_1D_parallel_Sweep(MeasurementScript):
+    
+    def run(self):
+        self.initialize()
+        dynamic_params = list()
+        for sweep in self.dynamic_sweeps:
+            sweep._param.set(sweep.get_setpoints()[0])
+            dynamic_params.append(sweep.param)
+        time.sleep(5)
+            
+        
+        data = do1d_parallel(*tuple(self.gettable_channels),
+                            param_set=dynamic_params,
+                            setpoints = self.dynamic_sweeps[0].get_setpoints(),
+                            delay = self.dynamic_sweeps[0]._delay,
+                            measurement_name=self.metadata.measurement.name or "measurement",
+                            break_condition = _interpret_breaks(self.break_conditions)
+                            )
+        
+        return data
+
