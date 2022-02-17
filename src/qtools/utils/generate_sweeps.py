c6be3cdc2b901cd38cd6e70f85f759cd029eff22
Sionludi Lab <sionludi@rwth.com>
2022-02-17 17:00:05 +0100

    Added convenience method to replace keywords in param dictionary

--- src/qtools/utils/generate_sweeps.py
+++ src/qtools/utils/generate_sweeps.py
@@ -18,3 +18,19 @@ def generate_sweep(start, stop, num_points, include_backward = False):
     else: 
         sweep = np.linspace(start, stop, num_points)
     return sweep
+
+def replace_parameter_settings(parameters: dict,
+                               old_val : str,
+                               new_value):
+    """
+    Replaces parameters based on their values with other value. Can be used to 
+    pass setpoint arrays to parameter-dicts created from json files without 
+    having to change the values by hand everytime.
+    """
+    for gate, param in parameters.items():
+        for x,y in param.items():
+            for name, val in y.items():
+                if val == old_val:
+                    parameters[gate][x][name]=new_value                    
+    return parameters
+    
