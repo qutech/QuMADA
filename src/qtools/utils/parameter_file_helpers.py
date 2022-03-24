eeb2a7c434e163dd0bf25c5b1c083eea4d4cdf9d
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2022-03-24 10:43:52 +0100

    Add ParameterDict class
    Overrides the | operator to correctly handle qtools' format.

--- src/qtools/utils/parameter_file_helpers.py
+++ src/qtools/utils/parameter_file_helpers.py
@@ -4,6 +4,7 @@ Created on Fri Mar 18 10:33:00 2022
 
 @author: Flash
 """
+import operator
 from typing import Mapping
 
 import numpy as np
@@ -12,86 +13,96 @@ import json
 import pandas as pd
 
 
-def update_dict_recursively(dct, new):
-    for key, val in new.items():
-        if isinstance(val, Mapping):
-            dct[key] = update_dict_recursively(dct.get(key, {}), val)
-        else:
-            dct[key] = val
-    return dct
+class ParameterDict(OrderedDict):
+
+    def __or__(self, other):
+        new = self.__class__(**self)
+        new |= other
+        return new
+
+    def __ior__(self, other):
+        for key, val in other.items():
+            # All low-level parameters have an entry "keys".
+            # Do not merge but overwrite those entries.
+            if isinstance(val, Mapping) and 'type' not in val.keys():
+                self[key] = operator.ior(self.get(key, ParameterDict()), val)
+            else:
+                self[key] = val
+        return self
 
 
 def update_parameters(parameter_file, new_parameters):
     existing_parameters = json.load(parameter_file.open('r' if parameter_file.exists() else 'x'),
-                                    object_pairs_hook=OrderedDict)
-    parameters = update_dict_recursively(existing_parameters, new_parameters)
+                                    object_pairs_hook=ParameterDict)
+    parameters = existing_parameters | new_parameters
     json.dump(parameters, parameter_file.open('w'), indent=2)
     return parameters
 
 
 def intialize_dac_parameter_file(mapping, parameter_file):
 
-    parameters = OrderedDict()
+    parameters = ParameterDict()
     for i in range(20):
         if i in mapping.keys():
             key = mapping[i]
         else:
             key = f"CH{i}"
-        parameters |= gettable_gate_entry(key)
+        parameters |= gettable_dac_entry(key)
 
     return update_parameters(parameter_file, parameters)
 
 
 def excel_to_dac_parameter_file(excel_file, parameter_file,
-                              gate_header='Sample', dac_header='DAC/AWG'):
-    excel = pd.read_excel(excel_file, usecols=[gate_header, dac_header])
-    mapping = {int(dac): sample for dac, sample in zip(excel['DAC/AWG'], excel['Sample'])
-               if not np.isnan(dac)}
+                                sample_header='Sample', dac_header='DAC/AWG'):
+    excel = pd.read_excel(excel_file, usecols=[sample_header, dac_header])
+    mapping = ParameterDict({int(dac): sample
+                             for dac, sample in zip(excel['DAC/AWG'], excel['Sample'])
+                             if not np.isnan(dac)})
 
     return intialize_dac_parameter_file(mapping, parameter_file)
 
 
-def dynamic_gate_entry(gate, start, stop, num_points=100, delay=0.025):
-    return {
-        gate: {
-            'voltage': {
+def dynamic_dac_entry(dac, start, stop, num_points=100, delay=0.025):
+    return ParameterDict({
+        dac: ParameterDict({
+            'voltage': ParameterDict({
                 'type': 'dynamic',
                 'start': start,
                 'stop': stop,
                 'num_points': num_points,
                 'delay': delay
-            }
-        }
-    }
+            })
+        })
+    })
 
 
-def static_gate_entry(gate, value):
-    return {
-        gate: {
-            'voltage': {
-                'type': 'static',
+def static_dac_entry(dac, value, gettable=False):
+    return ParameterDict({
+        dac: ParameterDict({
+            'voltage': ParameterDict({
+                'type': 'static gettable' if gettable else 'static',
                 'value': value
-            }
-        }
-    }
+            })
+        })
+    })
 
 
-def gettable_gate_entry(gate, **kwargs):
-    return {
-        gate: {
-            'voltage': {
+def gettable_dac_entry(dac, **kwargs):
+    return ParameterDict({
+        dac: ParameterDict({
+            'voltage': ParameterDict({
                 'type': 'gettable'
-            } | kwargs
-        }
-    }
+            }) | kwargs
+        })
+    })
 
 
-def static_smu_entry(smu, parameter, value):
-    return {
-        smu: {
-            parameter: {
-                'type': 'static',
+def static_smu_entry(smu, parameter, value, gettable=False):
+    return ParameterDict({
+        smu: ParameterDict({
+            parameter: ParameterDict({
+                'type': 'static gettable' if gettable else 'static',
                 'value': value
-            }
-        }
-    }
+            })
+        })
+    })
