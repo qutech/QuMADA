96e3670e16ba136665534512319d0fd6086c715c
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2022-07-07 14:41:40 +0200

    change argument in generate_sweep from include_backward to backsweep; formatting

--- src/qtools/utils/generate_sweeps.py
+++ src/qtools/utils/generate_sweeps.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Jan 27 16:27:19 2022
 
@@ -7,15 +6,16 @@ Created on Thu Jan 27 16:27:19 2022
 
 import numpy as np
 
-def generate_sweep(start, stop, num_points, include_backward = False):
+
+def generate_sweep(start, stop, num_points, backsweep=False):
     """
     Creates a linear array with equally spaced setpoints ranging from start to stop.
-    If include_backwars is set True, the array will contain twice as much 
+    If include_backwars is set True, the array will contain twice as much
     setpoints going from start to stop and back down to start again.
     """
-    if include_backward:
+    if backsweep:
         sweep = np.array((*np.linspace(start, stop, num_points), *np.linspace(stop, start, num_points)))
-    else: 
+    else:
         sweep = np.linspace(start, stop, num_points)
     return sweep
 
@@ -23,24 +23,27 @@ def replace_parameter_settings(parameters: dict,
                                old_val : str,
                                new_value):
     """
-    Replaces parameters based on their values with other value. Can be used to 
-    pass setpoint arrays to parameter-dicts created from json files without 
+    Replaces parameters based on their values with other value. Can be used to
+    pass setpoint arrays to parameter-dicts created from json files without
     having to change the values by hand everytime.
     """
     for gate, param in parameters.items():
         for x,y in param.items():
             for name, val in y.items():
                 if val == old_val:
-                    parameters[gate][x][name]=new_value                    
+                    parameters[gate][x][name] = new_value
     return parameters
 
-import copy    
+
+import copy
+
+
 def update_parameter_settings(parameters: dict,
                                old_val : str,
                                new_value):
     """
-    Replaces parameters based on their values with other value. Can be used to 
-    pass setpoint arrays to parameter-dicts created from json files without 
+    Replaces parameters based on their values with other value. Can be used to
+    pass setpoint arrays to parameter-dicts created from json files without
     having to change the values by hand everytime.
     Does not modify the original dict, but return a modified deep copy.
     """
@@ -49,9 +52,9 @@ def update_parameter_settings(parameters: dict,
         for x,y in param.items():
             for name, val in y.items():
                 if val == old_val:
-                    updated_parameters[gate][x][name]=new_value                    
+                    updated_parameters[gate][x][name] = new_value
     return updated_parameters
-    
+
 def parse_code_from_json(parameters):
     '''executes simple chunks of python code in strings starting with _'''
     updated_parameters = copy.deepcopy(parameters)
@@ -60,7 +63,6 @@ def parse_code_from_json(parameters):
             for name, val in y.items():
                 if isinstance(val, str):
                     if val[0] == "_":
-                        updated_parameters[gate][x][name]= eval(val[1:])  
-                        print((val))
+                        updated_parameters[gate][x][name] = eval(val[1:])
+                        print(val)
     return updated_parameters
-
