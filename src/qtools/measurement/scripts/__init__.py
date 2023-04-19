dff78f7a5d50f25b6dd0b0e5ce52c80b081d111e
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2023-04-19 10:37:48 +0200

    fix `__all__` in  `__init__.py`

--- src/qtools/measurement/scripts/__init__.py
+++ src/qtools/measurement/scripts/__init__.py
@@ -9,11 +9,11 @@ from .generic_measurement import (
 )
 
 __all__ = [
-    Generic_1D_Sweep,
-    Generic_1D_Sweep_buffered,
-    Generic_nD_Sweep,
-    Generic_1D_parallel_Sweep,
-    Generic_1D_parallel_asymm_Sweep,
-    Timetrace,
-    Timetrace_with_sweeps,
+    "Generic_1D_Sweep",
+    "Generic_1D_Sweep_buffered",
+    "Generic_nD_Sweep",
+    "Generic_1D_parallel_Sweep",
+    "Generic_1D_parallel_asymm_Sweep",
+    "Timetrace",
+    "Timetrace_with_sweeps",
 ]
