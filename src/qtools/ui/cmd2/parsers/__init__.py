8d4c55bf29495f6dcd7877dd4332c3735c7a3428
Daniel Grothe <daniel.grothe@rwth-aachen.de>
2023-05-22 17:36:02 +0200

    remove deprecated cmd2 console

--- src/qtools/ui/cmd2/parsers/__init__.py
+++
@@ -1,4 +0,0 @@
-from qtools.ui.cmd2.parsers.instrument_parser import InstrumentCommandSet
-from qtools.ui.cmd2.parsers.joblist_parser import JoblistCommandSet
-from qtools.ui.cmd2.parsers.measurement_parser import MeasurementCommandSet
-from qtools.ui.cmd2.parsers.metadata_parser import MetadataCommandSet
