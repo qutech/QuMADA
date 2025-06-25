30e0c113629b16ed11ed1e67de2991820b8537b9
Till Huckemann <T.Huckemann@gmail.com>
2025-06-25 15:06:08 +0200

    Added function to separate setpoint arrays based on the maximum difference of their values.

--- src/qumada/utils/generate_sweeps.py
+++ src/qumada/utils/generate_sweeps.py
@@ -21,6 +21,7 @@
 import copy
 
 import numpy as np
+from qumada.utils.load_from_sqlite_db import separate_up_down
 
 
 def generate_sweep(start, stop, num_points, backsweep=False):
@@ -77,3 +78,52 @@ def parse_code_from_json(parameters):
                         updated_parameters[gate][x][name] = eval(val[1:])
                         print(val)
     return updated_parameters
+
+def split_into_segments(setpoints, max_difference, segment_on_direction_change=True):
+    """
+    Used to split an array (mostly setpoints for sweeps) into multiple parts.
+    No segment contains differencens in the values of its setpoints that are
+    larger than max_difference. If segment_on_direction_change is True,
+    a separation at each change of monotony of the setpoints is enforced. This 
+    can help to separate ramps in different directions.
+    
+    
+
+    Parameters
+    ----------
+    setpoints : Array|List of floats
+        Setpoints to separate.
+    max_difference : Float
+        Maximum difference of setpoints in on segment.
+    segment_on_direction_change : Bool, optional
+        Whether a new segment should be enforced whenever the "direction"
+        of the setpoints changes. The default is True.
+
+    Returns
+    -------
+    segmented_setpoints : List[List[Floats]]
+        Resulting segmented setpoints.
+
+    """
+    segmented_setpoints = []
+    if segment_on_direction_change is True:
+        setpoints = separate_up_down(setpoints)[0]
+    else:
+        setpoints = [setpoints] # to allow equal treatment of both cases
+    i = 0
+    for sub_setpoints in setpoints:
+        segmented_setpoints.append([])
+        ref_value1 = sub_setpoints[0]
+        ref_value2 = sub_setpoints[0]
+        for setpoint in sub_setpoints:
+            if max(abs(setpoint-ref_value1), abs(setpoint-ref_value2)) > max_difference:
+                i+=1
+                ref_value1 = ref_value2 = setpoint
+                segmented_setpoints.append([])
+            segmented_setpoints[i].append(setpoint)
+            ref_value1 = max(segmented_setpoints[i])
+            ref_value2 = min(segmented_setpoints[i])
+                
+        i+=1
+    return segmented_setpoints
+    
