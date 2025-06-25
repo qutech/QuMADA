9901ab9f5b0e6e4154391d8525632c5dece00212
Till Huckemann <T.Huckemann@gmail.com>
2025-06-25 14:57:49 +0200

    Replaced gradients with diffs, improved grouping

--- src/qumada/utils/load_from_sqlite_db.py
+++ src/qumada/utils/load_from_sqlite_db.py
@@ -302,8 +302,9 @@ def separate_up_down(x_data, *y_data):
     """
     Separates data arrays/lists into multiple arrays depending on the direction of
     the x_data. Every change of monotony will result in a new sublist, e.g.
-    x_data = [1,2,3,2,1] would result in [[1,2,3], [2,1]]. All y_data data are 
-    separated at the same spots as the x_data. 
+    x_data = [1,2,3,2,1] would result in [[1,2,3], [2,1]]. In case the following datapoints
+    are the same, they will be included in the previous sublist. 
+    All y_data data are separated at the same spots as the x_data. 
 
     Parameters
     ----------
@@ -313,7 +314,7 @@ def separate_up_down(x_data, *y_data):
     Returns tuple: split x_data, split_y_data, list with sign of each segment.
 
     """
-    grad = np.gradient(x_data)
+    grad = np.diff(x_data)
     curr_sign = np.sign(grad[0])
     data_list_x = list()
     data_list_y = [list() for _ in y_data]
@@ -322,15 +323,23 @@ def separate_up_down(x_data, *y_data):
     start_helper = 0
     for i in range(0, len(grad)):
         if np.sign(grad[i]) != curr_sign and np.sign(grad[i]) != 0:
-            data_list_x.append(x_data[start_helper:i])
+            data_list_x.append(x_data[start_helper:i+1])
             for j in range(len(y_data)):
-                data_list_y[j].append(y_data[j][start_helper:i])
-            start_helper = i
-            curr_sign = np.sign(grad[i])
+                data_list_y[j].append(y_data[j][start_helper:i+1])
+            start_helper = i+1
+            try:
+                # i+1 as we want to include the datapoint into the next group if possible.
+                # Otherwise e.g. a sawtooth signal would always result in groups with single
+                # points just containing the point after the jump.
+                curr_sign = np.sign(grad[i+1])
+            except IndexError:
+                # Required as grad[i+1] is not defined for the last data point.
+                # A sign change here always results in a single point subgroup.
+                curr_sign = np.sign(grad[i])
             direction.append(curr_sign)
-    data_list_x.append(x_data[start_helper : len(grad)])
+    data_list_x.append(x_data[start_helper : len(grad)+1])
     for j in range(len(y_data)):
-        data_list_y[j].append(y_data[j][start_helper:len(grad)])
+        data_list_y[j].append(y_data[j][start_helper:len(grad)+1])
     if len(direction) == 0:
         direction.append(1)
     return data_list_x, *data_list_y, direction
