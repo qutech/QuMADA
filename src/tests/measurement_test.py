6afde6fed17a2f13674e3aa7d5a78015561a4bae
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2025-06-25 10:33:43 +0200

    Move test setup to conftest file

--- src/tests/measurement_test.py
+++ src/tests/measurement_test.py
@@ -42,35 +42,7 @@ from qumada.utils.load_from_sqlite_db import load_db
 from qumada.utils.ramp_parameter import *
 
 
-@dataclasses.dataclass
-class MeasurementTestData:
-    trigger: threading.Event
 
-    station: Station
-    dmm: DummyDmm
-    dac: DummyDac
-
-
-@pytest.fixture
-def measurement_test_data():
-    trigger = threading.Event()
-
-    # Setup qcodes station
-    station = Station()
-
-    # The dummy instruments have a trigger_event attribute as replacement for
-    # the trigger inputs of real instruments.
-
-    dmm = DummyDmm("dmm", trigger_event=trigger)
-    add_mapping_to_instrument(dmm, mapping=DUMMY_DMM_MAPPING)
-    station.add_component(dmm)
-
-    dac = DummyDac("dac", trigger_event=trigger)
-    add_mapping_to_instrument(dac, mapping=DummyDacMapping())
-    station.add_component(dac)
-
-    yield MeasurementTestData(trigger, station, dmm, dac)
-    station.close_all_registered_instruments()
 
 
 @pytest.fixture
@@ -104,27 +76,27 @@ def db():
         yield db_path
 
 
-def test_1d_buffered(measurement_test_data, buffer_settings, parameters, db):
+def test_1d_buffered(measurement_test_setup, buffer_settings, parameters, db):
     script = Generic_1D_Sweep_buffered()
     script.setup(
         parameters,
         metadata=None,
         buffer_settings=buffer_settings,
         trigger_type="hardware",
-        trigger_start=measurement_test_data.trigger.set,
-        trigger_reset=measurement_test_data.trigger.clear,
+        trigger_start=measurement_test_setup.trigger.set,
+        trigger_reset=measurement_test_setup.trigger.clear,
     )
 
     mapping = {
         "ohmic": {
-            "voltage": measurement_test_data.dmm.voltage,
-            "current": measurement_test_data.dmm.current,
+            "voltage": measurement_test_setup.dmm.voltage,
+            "current": measurement_test_setup.dmm.current,
         },
         "gate1": {
-            "voltage": measurement_test_data.dac.ch01.voltage,
+            "voltage": measurement_test_setup.dac.ch01.voltage,
         },
         "gate2": {
-            "voltage": measurement_test_data.dac.ch01.voltage,
+            "voltage": measurement_test_setup.dac.ch01.voltage,
         },
     }
     script.gate_parameters = mapping
